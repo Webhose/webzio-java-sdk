abdbd3af8e2367e87068ec15c7fc3ae392bf036e
Nemmar Ramos <nramos@vibalgroup.com>
2017-02-27 18:29:57 +0800

    Implement Apache URI Builder

--- src/com/webhoseio/sdk/WebhoseIOClient.java
+++ src/com/webhoseio/sdk/WebhoseIOClient.java
@@ -5,17 +5,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Map;
 
+import org.apache.http.client.utils.URIBuilder;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class WebhoseIOClient {
 
+	private static final String WEBHOSE_BASE_URL = "http://webhose.io";
 	private static WebhoseIOClient mClient;
+	private String mNext;
 	private String mApiKey;
-	
+
+	/**
+	 * Private constructor
+	 */
 	private WebhoseIOClient() {
 	}
 
@@ -31,38 +38,64 @@ public class WebhoseIOClient {
 		return mClient;
 	}
 
-	public JSONObject query(String endpoint,  Map<String, Object> queries) {
-		try {
-			String queryString = "";
-			
-			for(String key : queries.keySet()) {
-				queryString += String.format("%s=%s", key, queries.get(key));
-			}
-						
-			URL url = new URL(String.format("http://webhose.io/%s?token=%s&format=json&%s", endpoint, mApiKey, queryString));
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-		    connection.setDoOutput(true);
+	/**
+	 * Convenient method to fetch response from server
+	 * 
+	 * @param rawUrl Server URL
+	 * @return JSONObject Converted server response string 
+	 * @throws IOException
+	 * @throws URISyntaxException 
+	 */
+	public JSONObject getResponse(String rawUrl) throws IOException, URISyntaxException {
+		System.out.println("Connecting to url : " + rawUrl);
+		URL url = new URL(rawUrl);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setDoOutput(true);
 
-		    //Get Response  
-		    InputStream is = connection.getInputStream();
-		    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-		    StringBuilder response = new StringBuilder();
-		    String line;
-		    while ((line = rd.readLine()) != null) {
-		      response.append(line);
-		      response.append('\r');
-		    }
-		    rd.close();
-		    
-		    return new JSONObject(response.toString());
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+		// Get Response
+		InputStream is = connection.getInputStream();
+		BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+		StringBuilder response = new StringBuilder();
+		String line;
+		while ((line = rd.readLine()) != null) {
+			response.append(line);
+			response.append('\r');
+		}
+		rd.close();
+		
+		JSONObject o = new JSONObject(response.toString());
+		
+		// Set next query URL
+		mNext = WEBHOSE_BASE_URL + o.getString("next");
+
+		return o;
+	}
+
+	public JSONObject query(String endpoint, Map<String, String> queries) {
+		try {
+			URIBuilder builder = new URIBuilder(String.format("%s/%s?token=%s&format=json", WEBHOSE_BASE_URL, endpoint, mApiKey));
+			for (String key : queries.keySet()) {
+				builder.addParameter(key, queries.get(key));
+			}		
+			
+			return getResponse(builder.toString());
+		} catch (JSONException | IOException | URISyntaxException e) {
 			e.printStackTrace();
 		}
-		
 		return null;
-
+	}
+	
+	/**
+	 * Get next response of current request
+	 * @return JSONObject response
+	 */
+	public JSONObject getNext() {
+		try {
+			return  getResponse(mNext);
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
